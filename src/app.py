a97ec5c10016386a3933c279af70a0d3815f0ad7
Munachiso Elekwa <munaelekwa@gmail.com>
2022-06-13 12:04:46 +0100

    I have written a solution and corrected two tests.

--- src/app.py
+++ src/app.py
@@ -3,14 +3,27 @@ import os
 
 
 def prime(s):
-    # your code goes here
+    
+    for num in range(2, s//2):
+        if s > 1:
+            if s == 2:
+                print(f"{s} is a prime number")
+                return True
+            elif s % num == 0:
+                print(f"{s} is not a prime number")
+                return False
+            else:
+                print(f"{s} is a prime number")
+                return True
+        print(f"{s} is not a prime number")
 
 def solution(s):
     return prime(s)
 
 
+
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
+    if len(sys.argv) < 1:
         sys.exit(os.error("Argument required"))
 
-    print(solution(sys.argv[1]))
+    print(solution(int(sys.argv[1])))
